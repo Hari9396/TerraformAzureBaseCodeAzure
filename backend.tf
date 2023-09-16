f9e69e390353758c4081b44d6293a923d7ec6288
Harinath Reddy <harinath@resemblesystems.com>
2023-09-16 09:41:20 +0530

    Added NSG and Backend to the base code

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "devopsb30azterraformstate"
+    storage_account_name = "dvopsb31aztfstate"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
+}
