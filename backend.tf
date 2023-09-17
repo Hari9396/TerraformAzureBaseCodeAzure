fbed9a7ea82cb88034bda50a3dee3a2889d4f66b
Harinath Reddy <harinath@resemblesystems.com>
2023-09-17 11:55:22 +0530

    changed backend with new subscription

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "7882a27b-df79-4a75-b766-42a49c8e2ddc"
     resource_group_name  = "devopsb30azterraformstate"
-    storage_account_name = "dvopsb31aztfstate"
+    storage_account_name = "devopsb31aztfstate2"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
